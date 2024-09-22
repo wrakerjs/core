56348a8f5b316c785d5353e6b4b45ff9826575c6
josselinonduty <contact@josselinonduty.fr>
2024-09-22 11:56:16 +0200

    chore(docs): add docs for WrakerHeaders

--- src/common/WrakerHeaders.ts
+++ src/common/WrakerHeaders.ts
@@ -1,4 +1,21 @@
+/**
+ * A class that extends the native Map to handle HTTP headers with case-insensitive keys.
+ *
+ * @example
+ * const headers = new WrakerHeaders({
+ *  "Content-Type": "application/json",
+ * });
+ *
+ * headers.get("content-type"); // "application/json"
+ * headers.has("content-type"); // true
+ * headers.has("Content-Type"); // true
+ */
 export class WrakerHeaders extends Map<string, string> {
+  /**
+   * Creates an instance of WrakerHeaders.
+   *
+   * @param entries - An optional record of key-value pairs to initialize the headers.
+   */
   constructor(entries?: Record<string, string> | null) {
     super();
 
@@ -8,22 +25,53 @@ export class WrakerHeaders extends Map<string, string> {
       }
     }
   }
+
+  /**
+   * Retrieves the value associated with the given header name.
+   *
+   * @param name - The name of the header.
+   * @returns The value of the header, or undefined if the header does not exist.
+   */
   public get(name: string): any {
     return super.get(name.toLowerCase());
   }
 
+  /**
+   * Sets the value for a given header name.
+   *
+   * @param name - The name of the header.
+   * @param value - The value to set for the header.
+   * @returns The WrakerHeaders instance.
+   */
   public set(name: string, value: any): this {
     return super.set(name.toLowerCase(), value);
   }
 
+  /**
+   * Checks if a header with the given name exists.
+   *
+   * @param name - The name of the header.
+   * @returns True if the header exists, false otherwise.
+   */
   public has(name: string): boolean {
     return super.has(name.toLowerCase());
   }
 
+  /**
+   * Deletes a header with the given name.
+   *
+   * @param name - The name of the header.
+   * @returns True if the header was deleted, false otherwise.
+   */
   public delete(name: string): boolean {
     return super.delete(name.toLowerCase());
   }
 
+  /**
+   * Serializes the headers into a plain object.
+   *
+   * @returns A record containing all headers as key-value pairs.
+   */
   public serialize(): Record<string, string> {
     const result: Record<string, any> = {};
     for (const [key, value] of this) {
