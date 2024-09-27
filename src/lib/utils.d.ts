574a7f1e4999d6db73a90e307dd3c317944f39d3
josselinonduty <contact@josselinonduty.fr>
2024-09-27 22:34:05 +0200

    feat: add type utilities to extract keys and types in interface

--- src/lib/utils.d.ts
+++ src/lib/utils.d.ts
@@ -11,3 +11,9 @@ export type UnionToIntersection<U> = (
 ) extends (k: infer I) => void
   ? I
   : never;
+
+export type ExtractKeysByType<I, T> = {
+  [K in keyof I]-?: I[K] extends T | undefined ? K : never;
+}[keyof I];
+
+export type ExtractTypeByKey<I, K extends ExtractKeysByType<I, T>, T> = I[K];
